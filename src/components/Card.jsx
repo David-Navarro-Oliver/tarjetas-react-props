13c4f905cbb6a818dabe7f10fdb13603652f1a8e
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-18 00:13:56 +0100

    feat: render Card component in App

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,16 @@
 import "../styles/Card.css";
 
-function Card({ image, title, genre, description }) {
+export default function Card({ image, title, genre, description }) {
   return (
     <article className="card">
-      <img className="card__image" src={image} alt={`Portada de ${title}`} />
-
+      <img className="card__image" src={image} alt={title} />
       <div className="card__content">
         <h2 className="card__title">{title}</h2>
-
         <p className="card__text">
           <strong>Género:</strong> {genre}
         </p>
-
         <p className="card__text">{description}</p>
       </div>
     </article>
   );
 }
-
-export default Card;
