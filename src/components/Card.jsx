cfc9ba520ea6c8955f0765e0467e8cd492f65df3
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-17 23:39:47 +0100

    feat: add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import "../styles/Card.css";
+
+function Card({ image, title, genre, description }) {
+  return (
+    <article className="card">
+      <img className="card__image" src={image} alt={`Portada de ${title}`} />
+
+      <div className="card__content">
+        <h2 className="card__title">{title}</h2>
+
+        <p className="card__text">
+          <strong>Género:</strong> {genre}
+        </p>
+
+        <p className="card__text">{description}</p>
+      </div>
+    </article>
+  );
+}
+
+export default Card;
