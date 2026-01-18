1e234898a0e8c20464446016f24d0e83ed836d9b
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-18 01:34:57 +0100

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Animes recomendados</h1>
+      <p className="header__subtitle">
+        Una selección de animes imprescindibles para cualquier aficionado al género.
+      </p>
+    </header>
+  );
+}
